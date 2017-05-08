4ab3d3ee11a528decc1b799ffe1a780db01f8941
Jody Moore <jodymoore@g.harvard.edu>
2017-05-08 17:45:10 -0500

    reorder

--- app/Http/Controllers/reOrderController.php
+++ app/Http/Controllers/reOrderController.php
@@ -25,28 +25,36 @@ class reOrderController extends Controller
         // get login id
         $id = Auth::id(); 
 
-        $oldOrders = Orders::where('user_id','=',$id)->get();
+        $prevOrders = Orders::where('user_id','=',$id)->get();
 
-        $custOldOrders = [];
-        $total = [];
-        $id = [];
-
-        $count1 = 0;
-        $count2 = 0;
-        $count3 = 0;
-      
-
-        foreach($oldOrders as $value) {
-            $custOldOrders[$count1++] = $value->order;
-            $total[$count2++] = $value->total;
-            $id[$count3++] = $value->order_id;
-
+        dump($prevOrders);
+        foreach($prevOrders->products as $product) {
+            dump($product->topping);
+            dump($product->price);
         }
 
+        // #$oldOrders = Orders::where('user_id','=',$id)->get();
+
+        // $custOldOrders = [];
+        // $total = [];
+        // $id = [];
+
+        // $count1 = 0;
+        // $count2 = 0;
+        // $count3 = 0;
+      
+
+        // foreach($oldOrders as $value) {
+        //     $custOldOrders[$count1++] = $value->order;
+        //     $total[$count2++] = $value->total;
+        //     $id[$count3++] = $value->order_id;
+
+        // }
+
         return view('reorder')->with([
-            'orders' => $custOldOrders,
-            'total' => $total,
-            'id' => $id,
+            'orders' => $prevOrders,
+            // 'total' => $total,
+            // 'id' => $id,
         ]);
     }
 
@@ -58,6 +66,17 @@ class reOrderController extends Controller
         $id = $request->id;
 
         Cart::add($id, $order, $price, 1, array());
+
+        // Place order into Cart array format
+        Cart::add(array(
+            'id' => $id,
+            'name' => $order,
+            'price' => $price,
+            'quantity' => 1,
+            'attributes' => array(
+                'topping' => $topping,     
+          ),
+        ));
 
         Session::flash('message',$order.' was added to your order.');
 
